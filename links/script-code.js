660751bd43ab781b91ba770126f058dba868561c
tranchau1991 <tranchau1991@gmail.com>
2020-12-19 18:04:45 +0700

    add links manage

---
+++ links/script-code.js
@@ -0,0 +1,254 @@
+$(document).ready(function() {
+    $('#start-content').css('margin-top', 120);
+
+});
+
+var firebaseConfig = {
+    apiKey: "AIzaSyAuPuaJCP08o5JKW09F1XUWAy5GIiJ_cf8",
+    authDomain: "link-save-d79c8.firebaseapp.com",
+    databaseURL: "https://link-save-d79c8-default-rtdb.firebaseio.com",
+    projectId: "link-save-d79c8",
+    storageBucket: "link-save-d79c8.appspot.com",
+    messagingSenderId: "567380847437",
+    appId: "1:567380847437:web:eaca395ec37919f2f8a504",
+    measurementId: "G-RWXYFREXB0"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
+var user_ID = '';
+
+//invokes firebase authentication. 
+
+const auth = firebase.auth();
+document.querySelector("#show-register").addEventListener("click", () => {
+    showRegistration();
+});
+
+const showRegistration = () => {
+    document.querySelector("#registration-page").classList.remove("hide");
+    document.querySelector("#login-page").classList.add("hide");
+    document.querySelector("#homepage").classList.add("hide");
+    // document.querySelector(".show-signout").classList.add("hide");
+    $('.show-signout').hide()
+};
+
+document.querySelector("#show-login").addEventListener("click", () => {
+    showLogin();
+});
+
+const showLogin = () => {
+    document.querySelector("#registration-page").classList.add("hide");
+    document.querySelector("#login-page").classList.remove("hide");
+    document.querySelector("#homepage").classList.add("hide");
+    // document.querySelector(".show-signout").classList.add("hide");
+    $('.show-signout').hide()
+};
+
+document.querySelector(".signout").addEventListener("click", () => {
+    signOut();
+});
+
+const register = () => {
+    const email = document.querySelector("#registration-email").value;
+    const reemail = document.querySelector("#registration-reemail").value;
+    const password = document.querySelector("#registration-password").value;
+
+    if (email.trim() == "") {
+        alert("Enter Email");
+    } else if (password.trim().length < 7) {
+        alert("Password must be at least 7 characters");
+    } else if (email != reemail) {
+        alert("emails do not match");
+    } else {
+        auth
+            .createUserWithEmailAndPassword(email, password)
+            .catch(function(error) {
+                // Handle Errors here.
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                alert(errorMessage);
+                // ...
+            });
+    }
+};
+
+document.querySelector("#register").addEventListener("click", () => {
+    register();
+});
+
+//register when you hit the enter key
+document
+    .querySelector("#registration-password")
+    .addEventListener("keyup", (e) => {
+        if (event.keyCode === 13) {
+            e.preventDefault();
+
+            register();
+        }
+    });
+
+const login = () => {
+    const email = document.querySelector("#login-email").value;
+    const password = document.querySelector("#login-password").value;
+
+    if (email.trim() == "") {
+        alert("Enter Email");
+    } else if (password.trim() == "") {
+        alert("Enter Password");
+    } else {
+        authenticate(email, password);
+    }
+};
+
+document.querySelector("#login").addEventListener("click", () => {
+    login();
+});
+
+//sign in when you hit enter
+document
+    .querySelector("#login-password")
+    .addEventListener("keyup", (e) => {
+        if (event.keyCode === 13) {
+            e.preventDefault();
+
+            login();
+        }
+    });
+
+const authenticate = (email, password) => {
+    const auth = firebase.auth();
+    auth.signInWithEmailAndPassword(email, password);
+    firebase
+        .auth()
+        .signInWithEmailAndPassword(email, password)
+        .catch(function(error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            alert(errorMessage);
+        });
+};
+
+const showHomepage = () => {
+    document.querySelector("#registration-page").classList.add("hide");
+    document.querySelector("#login-page").classList.add("hide");
+    document.querySelector("#homepage").classList.remove("hide");
+    // document.querySelector(".show-signout").classList.remove("hide");
+    $('.show-signout').show()
+};
+
+const signOut = () => {
+    firebase
+        .auth()
+        .signOut()
+        .then(function() {
+            location.reload();
+        })
+        .catch(function(error) {
+            alert("error signing out, check network connection");
+        });
+};
+
+auth.onAuthStateChanged((firebaseUser) => {
+
+    if (firebaseUser) {
+        showHomepage();
+        user_ID = firebaseUser.uid
+        $('#email_login').html(firebaseUser.email)
+        loadData()
+    }
+});
+
+document
+    .querySelector("#forgot-password")
+    .addEventListener("click", () => {
+        const email = document.querySelector("#login-email").value;
+        if (email.trim() == "") {
+            alert("Enter Email");
+        } else {
+            forgotPassword(email);
+        }
+    });
+
+const forgotPassword = (email) => {
+    auth
+        .sendPasswordResetEmail(email)
+        .then(function() {
+            alert("email sent");
+        })
+        .catch(function(error) {
+            alert("invalid email or bad network connection");
+        });
+};
+
+// Ready to function
+
+// var config = {
+//     apiKey: "AIzaSyAuPuaJCP08o5JKW09F1XUWAy5GIiJ_cf8",
+// 	  authDomain: "link-save-d79c8.firebaseapp.com",
+// 	  databaseURL: "https://link-save-d79c8-default-rtdb.firebaseio.com",
+// 	  projectId: "link-save-d79c8",
+// 	  storageBucket: "link-save-d79c8.appspot.com",
+// 	  messagingSenderId: "567380847437",
+// 	  appId: "1:567380847437:web:eaca395ec37919f2f8a504",
+// 	  measurementId: "G-RWXYFREXB0"
+// };
+
+// firebase.initializeApp(config);
+
+//create firebase database reference
+var dbRef = firebase.database();
+var contactsRef = dbRef.ref('contacts');;
+
+function loadData() {
+    //load older conatcts as well as any newly added one...
+    contactsRef.orderByChild('userId').equalTo(user_ID).on("child_added", function(snap) {
+        console.log("added", snap.key, snap.val());
+        $('#contacts').append(contactHtmlFromObject(snap.val()));
+    });
+}
+
+//save contact
+$('.addValue').on("click", function(event) {
+    event.preventDefault();
+    if ($('#name').val() != '' || $('#email').val() != '') {
+        contactsRef.push({
+            name: $('#name').val().replace(/<[^>]*>/ig, ""),
+            email: $('#email').val().replace(/<[^>]*>/ig, ""),
+            location: {
+                city: $('#city').val().replace(/<[^>]*>/ig, ""),
+                state: $('#state').val().replace(/<[^>]*>/ig, ""),
+                zip: $('#zip').val().replace(/<[^>]*>/ig, "")
+            },
+            time: new Date().getTime(),
+            userId: user_ID
+        })
+        contactForm.reset();
+    } else {
+        alert('Please fill atlease name or email!');
+    }
+});
+
+//prepare conatct object's HTML
+function contactHtmlFromObject(contact) {
+    console.log(contact);
+    var html = '';
+    html += '<li class="list-group-item contact">';
+    html += '<div>';
+    if (contact.name != '') {
+        html += '<p class="lead">' + contact.name + '</p>';
+    }
+    html += '<p><a href="' + contact.email + '" target="_blank">' + contact.email + '</a></p>';
+    if (contact.location.city != '') {
+        html += '<p><small title="' + contact.location.zip + '">' + contact.location.city + ', ' + contact.location.state + '</small></p>';
+    }
+    if (contact.time != undefined) {
+        var d = new Date(contact.time);
+        html += '<p>' + d.toLocaleString() + '</p>';
+    }
+    html += '</div>';
+    html += '</li>';
+    return html;
+}
